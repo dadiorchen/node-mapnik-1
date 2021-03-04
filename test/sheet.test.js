6e695909cb33c3d6b52cef4927deb165e76086da
dadiorchen <dadiorchen@outlook.com>
2021-03-04 10:10:37 +0000

    feat: can connect to local postgresql

--- test/sheet.test.js
+++ test/sheet.test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 describe("sheet", async () => {
 
-  it("sheet", async () => {
+  it("world map", async () => {
 
     // register fonts and datasource plugins
     mapnik.register_default_fonts();
@@ -20,6 +20,33 @@ describe("sheet", async () => {
           im.encode('png', function(err,buffer) {
             if (err) throw err;
             fs.writeFile('/tmp/test/map.png',buffer, function(err) {
+              if (err) throw err;
+              console.log('saved map image to map.png');
+              res();
+            });
+          });
+        });
+      });
+    });
+  });
+
+  it.only("trees table map", async () => {
+
+    // register fonts and datasource plugins
+    mapnik.register_default_fonts();
+    mapnik.register_default_input_plugins();
+
+    var map = new mapnik.Map(256, 256);
+    const r = await new Promise((res, rej) => {
+      map.load('./test/postgis.xml', function(err,map) {
+        if (err) throw err;
+        map.zoomAll();
+        var im = new mapnik.Image(256, 256);
+        map.render(im, function(err,im) {
+          if (err) throw err;
+          im.encode('png', function(err,buffer) {
+            if (err) throw err;
+            fs.writeFile('/tmp/test/tree.png',buffer, function(err) {
               if (err) throw err;
               console.log('saved map image to map.png');
               res();
