cc7ff716e75e720c89ebb4ce2380343aee3be0fa
dadiorchen <dadiorchen@outlook.com>
2021-03-07 03:17:35 +0000

    feat: can reproduce the coordiate problem, use regualer point like: 0 0, 0 1, 1 0 to render on the map, can find the points on the map is irredular, do points on the map isn't even, although I know in mocarto it IS uneven, there are some point are cut when conjoint to another tile

--- greenstand/db.test.js
+++ greenstand/db.test.js
@@ -5,7 +5,7 @@ const expectRuntime = require("expect-runtime");
 
 describe("seed", () => {
 
-  it("seed", async () => {
+  it.skip("seed tree3, from real data", async () => {
     const db = knex({
       client: "pg",
       connection: {
@@ -35,6 +35,30 @@ describe("seed", () => {
       });
     }
     await db.raw("UPDATE trees3 SET estimated_geometric_location = ST_SetSRID(ST_MakePoint(lon, lat), 4326);");
+  }, 1000*60*5);
+
+  it("seed tree4, with fake data", async () => {
+    const db = knex({
+      client: "pg",
+      connection: {
+        host : '172.17.0.4',
+        port: 5432,
+        user : 'postgres',
+        password : 'greenstand',
+        database : 'treetracker'
+      },
+    });
+    await db("trees4").del();
+    expect(jsonData).toHaveProperty("features");
+    for(let lon = -179; lon < 180; lon += 7){
+      for(let lat = -80; lat < 80; lat += 7){
+        await db("trees4").insert({
+          lat,
+          lon,
+        });
+      }
+    }
+    await db.raw("UPDATE trees4 SET estimated_geometric_location = ST_SetSRID(ST_MakePoint(lon, lat), 4326);");
   }, 1000*60*5);
 
 });
