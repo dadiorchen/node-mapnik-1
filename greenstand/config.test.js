f8762c230c8373d8e20980412bfd812625a279a3
dadiorchen <dadiorchen@outlook.com>
2021-03-12 06:10:24 +0000

    feat: freetown config

--- greenstand/config.test.js
+++ greenstand/config.test.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const fs = require("fs");
-const config = require("./config");
+const {config, configFreetown} = require("./config");
 
 describe("", () => {
 
@@ -17,6 +17,22 @@ describe("", () => {
     expect(newContent).toMatch(/2222/);
     expect(newContent).toMatch(/namename/);
     expect(newContent).toMatch(/ppp/);
+
+//    const define = path.join(__dirname, 'stylesheet.xml');
+//    console.log("path:", define);
+//    mapInstance.load(define, {strict: true},function(err,_map) {
+  });
+
+  it.only("config freetown", async () => {
+    configFreetown();
+  const newDefine = path.join(__dirname, '../test/postgis.freetown.prod.xml');
+    const newContent = fs.readFileSync(newDefine).toString();
+    expect(newContent).toMatch(/newuser/);
+    expect(newContent).toMatch(/192.168/);
+    expect(newContent).toMatch(/2222/);
+    expect(newContent).toMatch(/namename/);
+    expect(newContent).toMatch(/ppp/);
+    expect(newContent).toMatch(/getEntityRelationshipChildren/);
 
 //    const define = path.join(__dirname, 'stylesheet.xml');
 //    console.log("path:", define);
