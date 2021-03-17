5ec28a27705e46beeedfdb2bb4e3a8df978f431e
dadiorchen <dadiorchen@outlook.com>
2021-03-17 21:21:04 +0000

    feat: half way test for grid scan

---
+++ greenstand/grid.test.js
@@ -0,0 +1,13 @@
+
+describe("grid", () => {
+
+  it("scan", async () => {
+    const x = 1424;
+    const y = 423;
+    const northEast = {
+      lat: 59.355596110016315, 
+      lng: 250.31250000000003
+    };
+    const southWest = {lat: -59.5343180010956, lng: -250.31250000000003};
+  });
+});
